57f9c996e9b9285ad2a238416c0b2235bf6b2012
neelarani <neela9624@gmail.com>
2025-01-15 16:11:58 +0600

    post req

--- index.js
+++ index.js
@@ -53,7 +53,17 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/users');
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin',
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // posts related api
     app.post('/posts', async (req, res) => {
